6742219d77b4193e39fa2dae09f13875ddd7357d
Shindo <=2026066@fsc-educ.com>
2026-06-09 16:54:03 +0900

    SQL作成

--- src/main/java/com/example/blog_app/BlogController.java
+++ src/main/java/com/example/blog_app/BlogController.java
@@ -1,10 +1,34 @@
 package com.example.blog_app;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
+
+@Controller
 public class BlogController {
+    
+    public BlogController(BlogService blogService){
+        this.blogService = blogService;
+    }
+    
     @GetMapping("/blogs")
+    //blogにきたらblog.htmlに移行
     public String blogs(){
         return "blogs";
     }
+
+    @GetMapping("/blogs/{id}")
+    public String idblog(){
+
+    }
+
+    @PostMapping("/post")
+    public String post(@RequestBody String entity) {
+        //TODO: process POST request
+        
+        return entity;
+    }
+    
 }
---
+++ src/main/java/com/example/blog_app/BlogService.java
@@ -0,0 +1,5 @@
+package com.example.blog_app;
+
+public class BlogService {
+    
+}
